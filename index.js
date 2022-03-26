246d2cf01d0459f9bef8f1ba941f1dd03d60f351
MrWhite1338 <aleksa.marjanov@outlook.com>
2022-03-25 21:43:09 -0500

    first page almost done

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const buttonaddon = document.querySelector('#button-addon2')
 const email = () => {
     alert('Check your email/junk you should have received a coupon')
 };
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiYWxla3NhbWFyamFub3YiLCJhIjoiY2wxNzg4OWdnNGNsdTNjcnB0eTUyaTFpZyJ9.Gb0b3LdcSTevZuB-w1ipCA';
+var map = new mapboxgl.Map({
+container: 'map',
+style: 'mapbox://styles/mapbox/streets-v11',
+center: [-73.990593,40.740121],
+zoom: 18,
+});
