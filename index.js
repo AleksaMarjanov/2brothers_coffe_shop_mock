de3d341e687363d44a60aedbdce5efe53cf8f73d
MrWhite1338 <aleksa.marjanov@outlook.com>
2022-03-27 16:48:29 -0500

    added menu page

--- index.js
+++ index.js
@@ -4,10 +4,18 @@ const email = () => {
     alert('Check your email/junk you should have received a coupon')
 };
 
+const apply = () => {
+    alert('We will get back to you as soon as possible')
+};
+
 mapboxgl.accessToken = 'pk.eyJ1IjoiYWxla3NhbWFyamFub3YiLCJhIjoiY2wxNzg4OWdnNGNsdTNjcnB0eTUyaTFpZyJ9.Gb0b3LdcSTevZuB-w1ipCA';
 var map = new mapboxgl.Map({
 container: 'map',
 style: 'mapbox://styles/mapbox/streets-v11',
-center: [-73.990593,40.740121],
-zoom: 18,
+center: [
+    21.2982868161137,
+    45.11944825
+  ],
+zoom: 18, 
 });
+
