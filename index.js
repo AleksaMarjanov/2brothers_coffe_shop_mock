1577bbc2802f635a5736f0f9c4e14f752c2b8719
MrWhite1338 <aleksa.marjanov@outlook.com>
2022-04-01 22:08:49 -0500

    optimized resolution of pictures

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-const buttonaddon = document.querySelector('#button-addon2')
-
-const email = () => {
-    alert('Check your email/junk you should have received a coupon')
-};
-
-const apply = () => {
-    alert('We will get back to you as soon as possible')
-};
-
 mapboxgl.accessToken = 'pk.eyJ1IjoiYWxla3NhbWFyamFub3YiLCJhIjoiY2wxNzg4OWdnNGNsdTNjcnB0eTUyaTFpZyJ9.Gb0b3LdcSTevZuB-w1ipCA';
 var map = new mapboxgl.Map({
 container: 'map',
